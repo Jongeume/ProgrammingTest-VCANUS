140c99626320aa1e22d6aff59f0f821096af519e
user <user@192.168.123.103>
2020-08-17 07:39:59 +0900

    refactor : Test_5 확장성 추가
    1. pondArr 배열 String타입에서 int타입으로 변경
    2. 멤버변수 ONE, TWO, THREE, FOUR 삭제
    3. Pond메서드를 삭제하고 멤버변수 pondArr에 값을 넣어 선언해주었다.
    4. setInit()메서드 를 setPond()로 바꿨다.
    5. getNumber메서드 하나로 줄여 재사용이 가능하도록 만들었다.
    6. updatePond() 추가하여 배열값 업데이트 기능을 구현주었다.
    setNumber를 해줄때마다 pondArr배열의 값이 업데이트 되기 때문에 연못의 수심깊이의 최대값 만큼 setNumber를
    하도록 조건식 t<=ROW/2인 반복문을 사용하였다.
    7. setNumber 역시 updatePond()의 t값마다 비교할 수심깊이 값과 늘어날 수심깊이 달라지도록 만들고 메서드를
    하나로 줄였다.
    8. 수심 깊이가 최대 5일 경우의 클래스 Pond_five를 추가하였다.
    
    관련 : #1

---
+++ ProgrammingTest/src/Test_5/Pond2.java
@@ -0,0 +1,202 @@
+package Test_5;
+
+public class Pond2 {
+
+	private static int ROW = 10;
+	private static int COL = 10;
+	private int pondArr[][] = new int[ROW][COL];
+	private int pondArr2[][] = new int[ROW][COL];
+
+	public static void main(String[] args) {
+
+		Pond2 pond = new Pond2();
+		pond.setPond();
+		pond.updatePond();
+		pond.printPond();
+		pond.depthSum();
+	}
+
+	//  초기값
+	private void setPond() {
+
+		for (int i = 1; i <= 9; i++) {
+
+			switch (i) {
+			case 1:
+				pondArr[i][4] = 1;
+				break;
+			case 2:
+				for (int j = 3; j <= 5; j++) {
+					pondArr[i][j] = 1;
+				}
+				break;
+			case 3:
+				for (int j = 1; j <= 6; j++) {
+					pondArr[i][j] = 1;
+				}
+				break;
+
+			case 4:
+				for (int j = 1; j <= 8; j++) {
+					pondArr[i][j] = 1;
+				}
+				break;
+			case 5:
+				for (int j = 1; j <= 8; j++) {
+					pondArr[i][j] = 1;
+				}
+				break;
+
+			case 6:
+				for (int j = 2; j <= 7; j++) {
+					pondArr[i][j] = 1;
+				}
+				break;
+
+			case 7:
+				for (int j = 3; j <= 6; j++) {
+					pondArr[i][j] = 1;
+				}
+				break;
+			case 8:
+				pondArr[i][4] = 1;
+				break;
+
+			}
+
+		}
+
+		// 초기값
+		for (int i = 0; i < ROW; i++) {
+			for (int j = 0; j < COL; j++) {
+				pondArr2[i][j] = pondArr[i][j];
+			}
+		}
+
+	}
+
+	// *************** 수정 시작 ***************
+
+	// 숫자 존재 여부 판단
+	private boolean exist1(int row, int col, int t) {
+		// ArrayIndexOutOfBoundsException 예방
+		if (row < 0 || row >= ROW || col < 0 || col >= COL) {
+			return false;
+		}
+		if (pondArr[row][col] == t) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean exist2(int row, int col, int t) {
+		if (row < 0 || row >= ROW || col < 0 || col >= COL) {
+			return false;
+		}
+		if (pondArr[row][col] == t + 1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// 해당 배열 기준 자기 자신을 제외한 상,하,좌,우 깊이를 비교하여 카운팅한다.
+	private int getNumber(int row, int col, int t) {
+		int mineCnt = 0;
+
+		if (exist1(row - 1, col, t))
+			mineCnt++;
+		if (exist1(row, col - 1, t))
+			mineCnt++;
+		if (exist1(row, col + 1, t))
+			mineCnt++;
+		if (exist1(row + 1, col, t))
+			mineCnt++;
+
+		if (exist2(row - 1, col, t))
+			mineCnt++;
+		if (exist2(row, col - 1, t))
+			mineCnt++;
+		if (exist2(row + 1, col, t))
+			mineCnt++;
+		if (exist2(row, col + 1, t))
+			mineCnt++;
+
+		int cnt = 0;
+		// 좌,우,상,하 모두 자신보다 깊이가 같거나 크면 현재 셀의 깊이 1 증가
+		if (mineCnt == 4) {
+			cnt = t + 1;
+		}
+		return cnt;
+	}
+
+	// 숫자 주입
+	private void setNumber(int row, int col, int t) {
+		if (pondArr[row][col] == t && getNumber(row, col, t) != 0) {
+			pondArr[row][col] = getNumber(row, col, t);
+		}
+	}
+
+	// 배열 값 업데이트
+	private void updatePond() {
+		for (int t = 1; t <= ROW / 2; t++) {
+			for (int i = 0; i < ROW; i++) {
+				for (int j = 0; j < COL; j++) {
+					setNumber(i, j, t);
+				}
+			}
+		}
+
+	}
+
+	// 연못 배열 출력
+	private void printPond() {
+		System.out.println("초기값");
+		for (int i = 0; i < ROW; i++) {
+			for (int j = 0; j < COL; j++) {
+				System.out.print(pondArr2[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		System.out.println("출력값");
+
+		for (int i = 0; i < ROW; i++) {
+			for (int j = 0; j < COL; j++) {
+				System.out.print(pondArr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+
+	// 연못의 깊이 총합
+	private void depthSum() {
+		int number[] = new int[5];
+
+		for (int i = 0; i < ROW; i++) {
+			for (int j = 0; j < COL; j++) {
+				for (int c = 1; c <= ROW / 2; c++) {
+					if (pondArr[i][j] == c) {
+						number[c - 1]++;
+					}
+				}
+			}
+		}
+
+		System.out.println(
+				"one: " + number[0] + ", two: " + number[1] + ", three: " + number[2] + ", four: " + number[3]);
+
+		System.out.println();
+		int result = 0;
+
+		for (int i = 1; i <= ROW / 2; i++) {
+			result += number[i - 1] * (i);
+		}
+		System.out.println("연못 깊이의 총합 : " + result);
+	}
+
+// *************** 수정 끝 **************
+}
